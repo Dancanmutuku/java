da262552b2b1641ba1b4b9a2c86173443e10776e
Dancanmutuku <133522453+Dancanmutuku@users.noreply.github.com>
2023-11-14 00:21:48 +0300

    corrected array

--- practise.java
+++ practise.java
@@ -6,7 +6,11 @@ public class practise {
         int[] numbers = new int[5];
         numbers[0]=1;
         numbers[1]=2;
-
+        //knowing there length
         System.out.println(Arrays.toString(numbers));
+        int [] numerics={2, 3, 4, 5, 6,};
+        // Array.sort(numerics);
+        // System.out.println(Array.toString(numerics));
+        System.out.println(numerics.length);
     }
 }
